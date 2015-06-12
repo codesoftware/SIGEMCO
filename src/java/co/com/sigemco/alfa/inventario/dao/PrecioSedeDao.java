4f4752bf04733d5197938610015fc2a65d5f7a87
jnsierra <jnsierrac@gmail.com>
2015-06-12 11:25:38 -0500

    12/06/2015 01
    Ajuste para la visualizacion de decimales al parametrizar precios

--- src/java/co/com/sigemco/alfa/inventario/dao/PrecioSedeDao.java
+++ src/java/co/com/sigemco/alfa/inventario/dao/PrecioSedeDao.java
@@ -41,7 +41,7 @@ public class PrecioSedeDao {
 
     public String obtienePreciosProductoPorSedeConFormatos() {
         String sql = "";
-        sql += "SELECT prpr_prpr, prpr_dska, to_char(prpr_precio,'9999999999.0') as prpr_precio , prpr_tius_crea, prpr_tius_update, \n ";
+        sql += "SELECT prpr_prpr, prpr_dska, to_char(prpr_precio,'9999999999.00') as prpr_precio , prpr_tius_crea, prpr_tius_update, \n ";
         sql += "       prpr_estado, to_char(prpr_fecha,'dd/mm/yyyy') as prpr_fecha, prpr_sede                                   \n ";
         sql += "  FROM in_tprpr                                                             \n ";
         sql += " WHERE prpr_sede = " + this.getSede_sede() + "\n";
