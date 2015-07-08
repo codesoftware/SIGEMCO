09f28c9e522ea613348370ae6acb12ab1fc2a7f3
jnsierra <jnsierrac@gmail.com>
2015-07-07 20:46:29 -0500

    07/07/2015
    Correccion para que corra el programa

---
+++ src/java/co/com/sigemco/alfa/inventario/action/ArqueoAction.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.com.sigemco.alfa.inventario.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ *
+ * @author nicolas
+ */
+public class ArqueoAction extends ActionSupport{
+    
+    public String insertar(){
+        return SUCCESS;
+    }
+    
+}
