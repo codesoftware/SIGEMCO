43fd1e95f5d7a6966039bf6bfbfc04e40c48b5d5
siscomputo <johnmorenoing@gmail.com>
2015-07-07 21:36:23 -0500

    Cambios arqueo

---
+++ src/java/co/com/sigemco/alfa/inventario/dao/ArqueoDao.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.com.sigemco.alfa.inventario.dao;
+
+/**
+ *
+ * @author john
+ */
+public class ArqueoDao {
+
+    private String arque_arque;
+    private String arque_fecha;
+    private String arque_comen;
+    private String arque_sede;
+    private String arque_valorf;
+    private String arque_valorc;
+    private String arque_difere;
+    private String arque_estado;
+
+    public String getArque_arque() {
+        return arque_arque;
+    }
+
+    public void setArque_arque(String arque_arque) {
+        this.arque_arque = arque_arque;
+    }
+
+    public String getArque_fecha() {
+        return arque_fecha;
+    }
+
+    public void setArque_fecha(String arque_fecha) {
+        this.arque_fecha = arque_fecha;
+    }
+
+    public String getArque_comen() {
+        return arque_comen;
+    }
+
+    public void setArque_comen(String arque_comen) {
+        this.arque_comen = arque_comen;
+    }
+
+    public String getArque_sede() {
+        return arque_sede;
+    }
+
+    public void setArque_sede(String arque_sede) {
+        this.arque_sede = arque_sede;
+    }
+
+    public String getArque_valorf() {
+        return arque_valorf;
+    }
+
+    public void setArque_valorf(String arque_valorf) {
+        this.arque_valorf = arque_valorf;
+    }
+
+    public String getArque_valorc() {
+        return arque_valorc;
+    }
+
+    public void setArque_valorc(String arque_valorc) {
+        this.arque_valorc = arque_valorc;
+    }
+
+    public String getArque_difere() {
+        return arque_difere;
+    }
+
+    public void setArque_difere(String arque_difere) {
+        this.arque_difere = arque_difere;
+    }
+
+    public String getArque_estado() {
+        return arque_estado;
+    }
+
+    public void setArque_estado(String arque_estado) {
+        this.arque_estado = arque_estado;
+    }
+    
+    
+
+}
